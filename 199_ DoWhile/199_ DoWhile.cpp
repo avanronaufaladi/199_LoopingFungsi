82a1fb9be2479aa4ff38b5728490c3a3a9d18f56
avanronaufaladi <avaroadi@gmail.com>
2024-03-19 11:05:08 +0700

    memanggil perulangan srand dan while

---
+++ 199_ DoWhile/199_ DoWhile.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	srand(time(0));
+
+	x = rand() % 10;
+
+	cout << "nilai awal : " << x << endl;
+	do
+	{
+		cout << "perulangan do.. while" << endl;
+		x = rand() % 10;
+
+		cout << "nilai x : " << x << endl;
+	} while (x > 4);
+}
